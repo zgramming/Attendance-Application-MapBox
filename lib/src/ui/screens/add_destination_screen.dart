afb8606c6931618e2ad0ba9e0e6621562421c0f0
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-06-24 21:19:24 +0700

    Separated TextField Search Destination

--- lib/src/ui/screens/add_destination_screen.dart
+++ lib/src/ui/screens/add_destination_screen.dart
@@ -1,184 +1,4 @@
-// import 'dart:async';
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-// import 'package:geolocator/geolocator.dart';
-// import 'package:global_template/global_template.dart';
-// import 'package:provider/provider.dart';
-
-// import '../../providers/maps_provider.dart';
-
-// import './widgets/pick_destination_screen/add_destination_form.dart';
-
-// class AddDestinationScreen extends StatefulWidget {
-//   static const String routeNamed = '/add-destination-screen';
-//   @override
-//   _AddDestinationScreenState createState() => _AddDestinationScreenState();
-// }
-
-// class _AddDestinationScreenState extends State<AddDestinationScreen> {
-//   final Completer<GoogleMapController> _controller = Completer();
-
-//   final TextEditingController _searchLocationController = TextEditingController();
-//   final TextEditingController _nameDestinationController = TextEditingController();
-
-//   double iconSize = 40;
-
-//   @override
-//   void dispose() {
-//     _searchLocationController.dispose();
-//     _nameDestinationController.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Tambah Lokasi'),
-//         actions: [
-//           InkWell(
-//             onTap: () => showModalBottomSheet<dynamic>(
-//               context: context,
-//               builder: (_) => AddDestinationForm(
-//                 nameDestinationController: _nameDestinationController,
-//               ),
-//             ),
-//             child: const Padding(
-//               padding: EdgeInsets.only(right: 16.0),
-//               child: Icon(
-//                 FontAwesomeIcons.check,
-//                 size: 18.0,
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//       body: Stack(
-//         children: [
-//           Selector<MapsProvider, Position>(
-//             selector: (_, provider) => provider.currentPosition,
-//             builder: (_, position, __) {
-//               return GoogleMap(
-//                 initialCameraPosition: CameraPosition(
-//                   target: LatLng(position.latitude, position.longitude),
-//                   zoom: 20,
-//                 ),
-//                 onMapCreated: (controller) async {
-//                   _controller.complete(controller);
-//                   await _gotToCenterUser();
-//                 },
-//                 onCameraMove: (position) =>
-//                     context.read<MapsProvider>().setTrackingCameraPosition(position),
-//                 onCameraIdle: () => print('Stop Camera'),
-//               );
-//             },
-//           ),
-//           Positioned(
-//             child: Icon(
-//               FontAwesomeIcons.mapMarkerAlt,
-//               color: colorPallete.primaryColor,
-//               size: iconSize,
-//             ),
-//             top: (sizes.height(context) - iconSize - kToolbarHeight) / 2.25,
-//             right: (sizes.width(context) - iconSize) / 2,
-//           ),
-// Align(
-//   alignment: Alignment.topCenter,
-//   child: Padding(
-//     padding: const EdgeInsets.only(top: kToolbarHeight, right: 40, left: 40),
-//     child: TextFormFieldCustom(
-//       controller: _searchLocationController,
-//       onSaved: (value) => print(value),
-//       onFieldSubmitted: (value) => _moveCameraByAddress(value),
-//       onChanged: _onChangedSearcLocation,
-//       prefixIcon: const Icon(
-//         FontAwesomeIcons.searchLocation,
-//         size: 18,
-//       ),
-//       suffixIcon: Selector<GlobalProvider, bool>(
-//         selector: (_, provider) => provider.isShowClearTextField,
-//         builder: (_, showClearTextField, __) {
-//           return showClearTextField
-//               ? IconButton(
-//                   icon: const Icon(FontAwesomeIcons.times),
-//                   onPressed: _clearSearchLocation,
-//                   iconSize: 18,
-//                   color: colorPallete.weekEnd,
-//                 )
-//               : const SizedBox();
-//         },
-//       ),
-//       textInputAction: TextInputAction.search,
-//       hintText: 'Cari Lokasi Absen...',
-//       disableOutlineBorder: false,
-//       hintStyle: appTheme.caption(context),
-//     ),
-//   ),
-// ),
-//         ],
-//       ),
-//     );
-//   }
-
-//   Future<void> _gotToCenterUser() async {
-//     final GoogleMapController controller = await _controller.future;
-//     await controller.animateCamera(
-//       CameraUpdate.newCameraPosition(
-//         CameraPosition(
-//           target: LatLng(
-//             context.read<MapsProvider>().currentPosition.latitude,
-//             context.read<MapsProvider>().currentPosition.longitude,
-//           ),
-//           zoom: 20,
-//         ),
-//       ),
-//     );
-//   }
-
-//   Future<void> _moveCameraByAddress(String address) async {
-//     try {
-//       final List<Placemark> placemark =
-//           await Geolocator().placemarkFromAddress(address.toLowerCase());
-//       if (placemark != null) {
-//         final GoogleMapController controller = await _controller.future;
-//         final Position position = placemark[0].position;
-//         await controller
-//             .animateCamera(CameraUpdate.newLatLng(LatLng(position.latitude, position.longitude)));
-//         print(placemark[0].toJson());
-//       } else {
-//         throw 'Destinasi Tidak Ditemukan';
-//       }
-//     } on PlatformException catch (err) {
-//       if (err.code == 'ERROR_GEOCODNG_ADDRESSNOTFOUND') {
-//         await globalF.showToast(
-//             message: 'Destinasi Tidak Ditemukan', isError: true, isLongDuration: true);
-//       } else {
-//         await globalF.showToast(message: err.code, isError: true, isLongDuration: true);
-//       }
-//     } catch (e) {
-//       await globalF.showToast(message: e.toString(), isError: true, isLongDuration: true);
-//     }
-//   }
-
-//   void _onChangedSearcLocation(String value) {
-//     final globalProvider = context.read<GlobalProvider>();
-//     if (value.isEmpty) {
-//       globalProvider.setShowClearTextField(false);
-//     } else {
-//       globalProvider.setShowClearTextField(true);
-//     }
-//   }
-
-//   void _clearSearchLocation() {
-//     _searchLocationController.clear();
-//     context.read<GlobalProvider>().setShowClearTextField(false);
-//   }
-// }
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:geolocator/geolocator.dart';
@@ -187,7 +7,8 @@ import 'package:latlong/latlong.dart';
 import 'package:provider/provider.dart';
 
 import '../../providers/maps_provider.dart';
-import './pick_destination_screen/add_destination_form.dart';
+import './widgets/pick_destination_screen/add_destination_form.dart';
+import './widgets/pick_destination_screen/input_text_search_destination.dart';
 
 class AddDestinationScreen extends StatefulWidget {
   static const routeNamed = '/add-destination-screen';
@@ -200,6 +21,7 @@ class _AddDestinationScreenState extends State<AddDestinationScreen> {
   final TextEditingController _searchLocationController = TextEditingController();
   final TextEditingController _nameDestinationController = TextEditingController();
   MapController _mapController;
+
   @override
   void initState() {
     super.initState();
@@ -251,14 +73,15 @@ class _AddDestinationScreenState extends State<AddDestinationScreen> {
                   onPositionChanged: (mapPositioned, hasGesture) {
                     print('Move Camera ${mapPositioned.center}');
                     Future.delayed(
-                        const Duration(seconds: 1),
-                        () =>
-                            context.read<MapsProvider>().setTrackingCameraPosition(mapPositioned));
+                      const Duration(seconds: 1),
+                      () => context.read<MapsProvider>().setTrackingCameraPosition(mapPositioned),
+                    );
                   },
                 ),
                 layers: [
                   TileLayerOptions(
-                    urlTemplate: 'YOUR MAPBOX URL',
+                    urlTemplate:
+                        'https://api.mapbox.com/styles/v1/zeffryy/ckbm42cwb124f1ipgndrdcz8p/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiemVmZnJ5eSIsImEiOiJja2JtM3hrOWMxZmJ0MnNvZDZ5b3FteXZvIn0.by1MdhnYRjFSDTClAVMNyg',
                     subdomains: ['a', 'b', 'c'],
                   ),
                   MarkerLayerOptions(
@@ -288,77 +111,13 @@ class _AddDestinationScreenState extends State<AddDestinationScreen> {
           ),
           Align(
             alignment: Alignment.topCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(top: kToolbarHeight, right: 40, left: 40),
-              child: TextFormFieldCustom(
-                controller: _searchLocationController,
-                onSaved: (value) => print(value),
-                onFieldSubmitted: (value) => _moveCameraByAddress(value),
-                onChanged: _onChangedSearcLocation,
-                prefixIcon: const Icon(
-                  FontAwesomeIcons.searchLocation,
-                  size: 18,
-                ),
-                suffixIcon: Selector<GlobalProvider, bool>(
-                  selector: (_, provider) => provider.isShowClearTextField,
-                  builder: (_, showClearTextField, __) {
-                    return showClearTextField
-                        ? IconButton(
-                            icon: const Icon(FontAwesomeIcons.times),
-                            onPressed: _clearSearchLocation,
-                            iconSize: 18,
-                            color: colorPallete.weekEnd,
-                          )
-                        : const SizedBox();
-                  },
-                ),
-                textInputAction: TextInputAction.search,
-                hintText: 'Cari Lokasi Absen...',
-                disableOutlineBorder: false,
-                hintStyle: appTheme.caption(context),
-              ),
+            child: InputTextSearchAddress(
+              searchLocationController: _searchLocationController,
+              mapController: _mapController,
             ),
           ),
         ],
       ),
     );
-  }
-
-  Future<void> _moveCameraByAddress(String address) async {
-    try {
-      final List<Placemark> placemark =
-          await Geolocator().placemarkFromAddress(address.toLowerCase());
-      if (placemark != null) {
-        final Position position = placemark[0].position;
-        _mapController.move(LatLng(position.latitude, position.longitude), 18);
-
-        print(placemark[0].toJson());
-      } else {
-        throw 'Destinasi Tidak Ditemukan';
-      }
-    } on PlatformException catch (err) {
-      if (err.code == 'ERROR_GEOCODNG_ADDRESSNOTFOUND') {
-        await globalF.showToast(
-            message: 'Destinasi Tidak Ditemukan', isError: true, isLongDuration: true);
-      } else {
-        await globalF.showToast(message: err.code, isError: true, isLongDuration: true);
-      }
-    } catch (e) {
-      await globalF.showToast(message: e.toString(), isError: true, isLongDuration: true);
-    }
-  }
-
-  void _onChangedSearcLocation(String value) {
-    final globalProvider = context.read<GlobalProvider>();
-    if (value.isEmpty) {
-      globalProvider.setShowClearTextField(false);
-    } else {
-      globalProvider.setShowClearTextField(true);
-    }
-  }
-
-  void _clearSearchLocation() {
-    _searchLocationController.clear();
-    context.read<GlobalProvider>().setShowClearTextField(false);
   }
 }
---
+++ lib/src/ui/screens/widgets/pick_destination_screen/input_text_search_destination.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:latlong/latlong.dart';
+import 'package:global_template/global_template.dart';
+import 'package:mapbox_search/mapbox_search.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../providers/maps_provider.dart';
+
+class InputTextSearchAddress extends StatelessWidget {
+  const InputTextSearchAddress({
+    @required this.mapController,
+    @required this.searchLocationController,
+  });
+
+  final TextEditingController searchLocationController;
+  final MapController mapController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: kToolbarHeight, right: 40, left: 40),
+      child: Material(
+        elevation: 10.0,
+        child: TypeAheadFormField<MapBoxPlace>(
+          suggestionsCallback: (pattern) async {
+            final globalProvider = context.read<GlobalProvider>();
+            if (pattern.isEmpty) {
+              Future.delayed(const Duration(milliseconds: 500),
+                  () => globalProvider.setShowClearTextField(false));
+              return [];
+            } else {
+              Future.delayed(const Duration(milliseconds: 500),
+                  () => globalProvider.setShowClearTextField(true));
+              return await context
+                  .read<MapsProvider>()
+                  .getAutocompleteMapbox(query: pattern, apiKey: AppConfig.mapBoxApiKey);
+            }
+          },
+          itemBuilder: (context, itemData) {
+            return ListTile(
+              leading: const Icon(FontAwesomeIcons.marker),
+              title: Text(itemData.placeName),
+            );
+          },
+          onSuggestionSelected: (suggestion) async =>
+              await _moveCameraByAddress(suggestion.placeName),
+          errorBuilder: (context, error) {
+            return Center(child: Text(error.toString()));
+          },
+          noItemsFoundBuilder: (context) {
+            return const Text('Lokasi Tidak Ditemukan');
+          },
+          hideOnError: true,
+          textFieldConfiguration: TextFieldConfiguration(
+            controller: searchLocationController,
+            decoration: InputDecoration(
+              hintStyle: appTheme.caption(context),
+              hintText: 'Cari Lokasi Absen',
+              filled: true,
+              fillColor: colorPallete.white,
+              // border: InputBorder.none,
+              border: InputBorder.none,
+              prefixIcon: const Icon(FontAwesomeIcons.searchLocation, size: 18),
+              suffixIcon: Selector<GlobalProvider, bool>(
+                selector: (_, provider) => provider.isShowClearTextField,
+                builder: (_, showClearTextField, __) {
+                  return showClearTextField
+                      ? IconButton(
+                          icon: const Icon(FontAwesomeIcons.times),
+                          onPressed: () => _clearSearchLocation(context),
+                          iconSize: 18,
+                          color: colorPallete.weekEnd,
+                        )
+                      : const SizedBox();
+                },
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _moveCameraByAddress(String address) async {
+    try {
+      final List<Placemark> placemark =
+          await Geolocator().placemarkFromAddress(address.toLowerCase());
+      if (placemark != null) {
+        final Position position = placemark[0].position;
+        mapController.move(LatLng(position.latitude, position.longitude), 18);
+
+        print(placemark[0].toJson());
+      } else {
+        throw 'Destinasi Tidak Ditemukan';
+      }
+    } on PlatformException catch (err) {
+      if (err.code == 'ERROR_GEOCODNG_ADDRESSNOTFOUND') {
+        await globalF.showToast(
+            message: 'Destinasi Tidak Ditemukan', isError: true, isLongDuration: true);
+      } else {
+        await globalF.showToast(message: err.code, isError: true, isLongDuration: true);
+      }
+    } catch (e) {
+      await globalF.showToast(message: e.toString(), isError: true, isLongDuration: true);
+    }
+  }
+
+  void _clearSearchLocation(BuildContext context) {
+    searchLocationController.clear();
+    context.read<GlobalProvider>().setShowClearTextField(false);
+  }
+}
